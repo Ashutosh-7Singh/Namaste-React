dafd51c3c5b2443d372df81473a9bb29ba62fb94
Ashutosh-7Singh <ashutosh.singh@thegoacademy.com>
2024-11-07 00:12:55 +0530

    update

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+const newLocal = "I am in h1 tag";
 const parent = React.createElement(
     "div",
     { id: "parent" },
    [ React.createElement(
       "div",
       { id: "child" },
-    [  React.createElement("h1", {}, "I am in h1 tag"), React.createElement("h2", {}, "I am in h2 tag")]
+    [  React.createElement("h1", {}, "this is namaste react  ❤️"), React.createElement("h2", {}, "I am in h2 tag")]
     ),
     React.createElement(
         "div",
         { id: "child2" },
-      [  React.createElement("h1", {}, "I am in h1 tag"), React.createElement("h2", {}, "I am in h2 tag")]
+      [  React.createElement("h1", {}, newLocal), React.createElement("h2", {}, "I am in h2 tag")]
       )]
   );
   
--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Project Title 🚀
 
 ## Features
-- 🚀 Fast and efficient
-- :rocket: Ready for launch
+- 🚀 Fast and efficient 
+- npm init
+- git init
+- git branch -m master main
+- git add .
+- git commit -m "Initial commit"
+- git remote add origin https://github.com/Ashutosh-7Singh/namaste-react.git
+- git push -u origin main
+- npm i -D parcel
+- .gitignore (node_module)
+- npm i react
+- npm i react-dom
+
+❤️ What Parcel Does For you 
+
